61e86d9f1d4be4ac46b5397fe426d63ab3e65ea5
dodo <aseaways1234@gail.com>
2021-09-24 12:00:58 +0700

    init react file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Container} from './components/styles/Container.styled'
+import { Header } from './components/Header';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Container>
+        <h1>Hello World</h1>
+    </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <h1>Header</h1>
+        </StyledHeader>
+    )
+}
